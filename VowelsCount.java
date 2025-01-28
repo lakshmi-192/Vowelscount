10a11844d893c9876dea379e1491298dc5235122
lakshmi-192 <koyya.lakshmi12@gmail.com>
2025-01-28 19:20:13 +0530

    Add files via upload
    Count of vowels in a string

---
+++ VowelsCount.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class VowelsCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
